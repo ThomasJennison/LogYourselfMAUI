a9ce505e8728d64bb3c9c391595b5fe70005c98d
Thomas Jennison <thomas.a.jennison@gmail.com>
2022-01-24 14:04:19 -0500

    Add project files.

---
+++ LogYourselfMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LogYourselfMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
