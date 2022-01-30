08e89a644da34c4b70d9ca5c5b0b075bc1f9feb5
Thomas Jennison <thomas.a.jennison@gmail.com>
2022-01-30 16:31:44 -0500

    More frontend migration changes

--- LogYourselfMAUI/Views/DailyTrendsPage.xaml.cs
+++ LogYourselfMAUI/Views/DailyTrendsPage.xaml.cs
@@ -17,21 +17,21 @@ namespace LogYourselfMAUI.Views
 
         private void View_model_TrendsUpdated(object sender, DailyTrendsSelectedEventArgs e)
         {
-            List<OccuranceModel> selectedTrendOccurances = e.SelectedTrend.Occurances;
-            List<ChartEntry> chartItems = new List<ChartEntry>();
-            foreach (OccuranceModel occurance in selectedTrendOccurances)
-            {
-                chartItems.Add(
-                    new ChartEntry((float)occurance.Ammount)
-                    {
-                        Label = occurance.Time.ToString("h:m:tt"),
-                        ValueLabel = occurance.Ammount.ToString("0.00"),
-                        Color = SKColor.Parse(occurance.Ammount < 5 ? "#ff3f38" : "#65fa43")
-                    });
-            }
+            //List<OccuranceModel> selectedTrendOccurances = e.SelectedTrend.Occurances;
+            //List<ChartEntry> chartItems = new List<ChartEntry>();
+            //foreach (OccuranceModel occurance in selectedTrendOccurances)
+            //{
+            //    chartItems.Add(
+            //        new ChartEntry((float)occurance.Ammount)
+            //        {
+            //            Label = occurance.Time.ToString("h:m:tt"),
+            //            ValueLabel = occurance.Ammount.ToString("0.00"),
+            //            Color = SKColor.Parse(occurance.Ammount < 5 ? "#ff3f38" : "#65fa43")
+            //        });
+            //}
 
-            LineChart chart = new LineChart { Entries = chartItems.Count > 0 ? chartItems.ToArray() : null };
-            chartView.Chart = chart;
+            //LineChart chart = new LineChart { Entries = chartItems.Count > 0 ? chartItems.ToArray() : null };
+            //chartView.Chart = chart;
         }
     }
 }
